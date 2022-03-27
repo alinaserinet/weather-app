31ff2489e8b66c70e396b176662c41c6895ce386
Ali Naseri <mo.ali.naseri@gmail.com>
2022-03-27 22:00:24 +0430

    add default values for api urls

--- src/services/http.js
+++ src/services/http.js
@@ -1,7 +1,13 @@
 import axios from 'axios'
 
+const baseURL =
+  process.env.REACT_APP_BASE_URL || 'https://api.openweathermap.org/data/2.5/'
+
+const geoURL =
+  process.env.REACT_APP_GEO_URL || 'https://api.openweathermap.org/geo/1.0/'
+
 const http = axios.create({
-  baseURL: process.env.REACT_APP_BASE_URL,
+  baseURL,
   params: {
     appid: process.env.REACT_APP_TOKEN,
     units: process.env.REACT_APP_UNITS,
@@ -9,7 +15,7 @@ const http = axios.create({
 })
 
 export const http2 = axios.create({
-  baseURL: process.env.REACT_APP_GEO_URL,
+  baseURL: geoURL,
   params: {
     appid: process.env.REACT_APP_TOKEN,
   },
